9cacaaa5413e2833731876f19ac5da0d203c02a2
yjqiang <13307130285@fudan.edu.cn>
2018-09-25 18:03:48 +0800

    集合webhub

--- fetch_roomids/async_fetch_roomid.py
+++ fetch_roomids/async_fetch_roomid.py
@@ -5,81 +5,10 @@ save_one 50w分批次运行，400一个批次，全部运行50w后，保存toml
 输出文件为[(roomid, uid), (roomid, uid) ...]
 '''
 import asyncio
-import aiohttp
 import sys
 import toml
-import json
 import os
-
-
-class WebHub:
-    
-    def __init__(self):
-        self.var_session = None
-
-    @property
-    def session(self):
-        if self.var_session is None:
-            self.var_session = aiohttp.ClientSession()
-        return self.var_session
-    
-    async def get_json_rsp(self, rsp, url):
-        if rsp.status == 200:
-            # json_response = await response.json(content_type=None)
-            data = await rsp.read()
-            json_rsp = json.loads(data)
-            if isinstance(json_rsp, dict) and 'code' in json_rsp:
-                code = json_rsp['code']
-                if code == 1024:
-                    print('b站炸了，暂停所有请求1.5s后重试，请耐心等待')
-                    await asyncio.sleep(1.5)
-                    return None
-                elif code == 3:
-                    print('api错误，稍后重试，请反馈给作者')
-                    await asyncio.sleep(1)
-                    return None
-            return json_rsp
-        elif rsp.status == 403:
-            print('403频繁', url)
-        return None
-        
-    async def session_get(self, url, headers=None, data=None, params=None):
-        while True:
-            try:
-                async with self.session.get(url, headers=headers, data=data, params=params) as response:
-                    json_rsp = await self.get_json_rsp(response, url)
-                    if json_rsp is not None:
-                        return json_rsp
-            except:
-                # print('当前网络不好，正在重试，请反馈开发者!!!!')
-                print(sys.exc_info()[0], sys.exc_info()[1], url)
-                continue
-    
-    async def fetch_follow_num(self, uid):
-        url = f'https://api.live.bilibili.com/relation/v1/Feed/GetUserFc?follow={uid}'
-        json_rsp = await self.session_get(url)
-        if not json_rsp['code'] and 'fc' in json_rsp['data']:
-            # int
-            return json_rsp['data']['fc']
-        else:
-            print(f'{uid}的用户获取过程中发生错误，{json_rsp}')
-    
-    async def fetch_room_info(self, roomid):
-        url = f'https://api.live.bilibili.com/room/v1/Room/room_init?id={roomid}'
-        json_rsp = await self.session_get(url)
-        if not json_rsp['code']:
-            data = json_rsp['data']
-            short_id = data["short_id"]
-            if short_id and short_id == roomid:
-                # print(data)
-                return None, None
-            # int, int
-            return data['room_id'], data['uid']
-        elif json_rsp['code'] == 60004:
-            return None, None
-        else:
-            print(f'{roomid}的房间获取过程中发生错误，{json_rsp}')
-            sys.exit(-1)
+from bili_web import WebHub
 
 
 async def save_one(room_min, room_max):
@@ -99,6 +28,7 @@ async def save_one(room_min, room_max):
             results = await asyncio.gather(*tasklist)
             for real_roomid, uid in results:
                 if real_roomid is not None:
+                    print(real_roomid)
                     list_rooms.append((real_roomid, uid))
         print(f'当前一共{len(list_rooms)}个房间({room_min}-{i+step-1})')
 
